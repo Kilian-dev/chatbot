509112a33c25795094c5ef364bbb31754d4690b5
Kilian-dev <72600197+Kilian-dev@users.noreply.github.com>
2025-01-22 14:01:24 +0100

    Updates

---
+++ precache-manifest.ac336e55ae24ab45e15c452f2db6faa9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aca4c97fbefec695c79c8d1f3561be5",
+    "url": "/Kilian-dev/chatbot/index.html"
+  },
+  {
+    "revision": "1dac2ec0c89aa7d7e538",
+    "url": "/Kilian-dev/chatbot/static/css/main.f92a1588.chunk.css"
+  },
+  {
+    "revision": "046287804c082299008a",
+    "url": "/Kilian-dev/chatbot/static/js/2.7186c6ed.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/Kilian-dev/chatbot/static/js/2.7186c6ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc7818e1212268e630a",
+    "url": "/Kilian-dev/chatbot/static/js/3.778602a0.chunk.js"
+  },
+  {
+    "revision": "1dac2ec0c89aa7d7e538",
+    "url": "/Kilian-dev/chatbot/static/js/main.ae872c41.chunk.js"
+  },
+  {
+    "revision": "e81c15fa1a96834a68bc",
+    "url": "/Kilian-dev/chatbot/static/js/runtime-main.f05b4b0e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Kilian-dev/chatbot/static/media/logo.06e73328.svg"
+  }
+]);
